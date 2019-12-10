b276b89c938d6ffa3418748ba2bce1911694a4a9
Hossein <h.rezaie8860@gmail.com>
2019-12-10 17:39:27 +0330

    state 06 create other class

---
+++ app/src/main/java/com/teta_tm/bizban/accountingapp/InvoiceActivityClass.java
@@ -0,0 +1,14 @@
+package com.teta_tm.bizban.accountingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InvoiceActivityClass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invoice_class);
+    }
+}
