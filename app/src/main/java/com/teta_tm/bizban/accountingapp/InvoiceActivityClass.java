5bb6dd9dcd0f9f8a1eacec5600a27b71c7227b54
Hossein <h.rezaie8860@gmail.com>
2019-12-15 16:27:22 +0330

    state 07 invoice xml and java

--- app/src/main/java/com/teta_tm/bizban/accountingapp/InvoiceActivityClass.java
+++ app/src/main/java/com/teta_tm/bizban/accountingapp/InvoiceActivityClass.java
@@ -3,12 +3,52 @@ package com.teta_tm.bizban.accountingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class InvoiceActivityClass extends AppCompatActivity {
+    ImageView nvcCls,nvcAddCstmr;
+    TextView nvcNo,nvcTyp,nvcDate;
+    EditText setCstmr;
+    Button nvcAddPrdct,nvcDesc,nvcOk,nvcDscnt,nvcAddition,nvcDdct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invoice_class);
+        inIt();
     }
+
+
+    public void inIt(){
+        nvcCls= findViewById(R.id.img_invc_cls);
+        nvcAddCstmr= findViewById(R.id.img_add_cstmr);
+        nvcNo=findViewById(R.id.tv_invc_no);
+        nvcTyp=findViewById(R.id.tv_invc_typ);
+        nvcDate= findViewById(R.id.tv_invc_date);
+        setCstmr= findViewById(R.id.ed_set_cstmr);
+        nvcAddPrdct= findViewById(R.id.btn_add_prdct);
+        nvcDesc= findViewById(R.id.btn_nvc_dsc);
+        nvcOk= findViewById(R.id.btn_nvc_ok);
+        nvcDscnt= findViewById(R.id.btn_nvc_dcnt);
+        nvcAddition= findViewById(R.id.btn_nvc_addition);
+        nvcDdct= findViewById(R.id.btn_nvc_ddct);
+
+    }
+
+    public void closeNvc(){
+
+    }
+
+    public void setDate(){
+
+    }
+
+    public void setCstmr(){
+
+
+    }
+
 }
--- app/src/main/java/com/teta_tm/bizban/accountingapp/MainActivity.java
+++ app/src/main/java/com/teta_tm/bizban/accountingapp/MainActivity.java
@@ -1,5 +1,8 @@
 package com.teta_tm.bizban.accountingapp;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
@@ -15,17 +18,55 @@ import com.teta_tm.bizban.accountingapp.Utils.DbHelper;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
+    private Button income,received,sell,cost,pay,buy,
+            installment,check,bnk_acnt,customer,edit,wrhus_prdct,report;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inIt();
+        sellInvoice();
 
     }
 
 
+    public void inIt(){
+
+        income=findViewById(R.id.btn_income);
+        received=findViewById(R.id.btn_receive);
+        sell=findViewById(R.id.btn_sell);
+        cost=findViewById(R.id.btn_cost);
+        pay=findViewById(R.id.btn_pay);
+        buy=findViewById(R.id.btn_buy);
+        installment=findViewById(R.id.btn_installment);
+        check=findViewById(R.id.btn_checks);
+        bnk_acnt=findViewById(R.id.btn_bnk_acnt);
+        customer=findViewById(R.id.btn_customer);
+        edit=findViewById(R.id.btn_edit);
+        wrhus_prdct=findViewById(R.id.btn_wrhus_prdct);
+        report=findViewById(R.id.btn_report);
+
+    }
+
+
+
+    public void sellInvoice(){
+
+        sell.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(getApplicationContext(),InvoiceActivityClass.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+    public void invoiceDialog(){
+
+
+
+    }
 
 
 }
